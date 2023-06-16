cff575f3b497d21b7ed833dcbbe4dd08372da96c
Alexadr45 <115409750+Alexadr45@users.noreply.github.com>
2023-06-16 23:03:31 +0500

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -58,9 +58,6 @@ def main():
     if len(duplicateRows) > 0:
         df = df.drop_duplicates().reset_index(drop=True)
 
-    # Удаление лишних столбцов
-    df = df[['Snippet', 'Target']]
-
     # Делим данные на тренировочную, тестовую и валидационную выборки
     train, test = train_test_split(df, test_size=0.3, random_state=42)
     test, val = train_test_split(test, test_size=0.5, random_state=42)
