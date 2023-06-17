6c708ee831b30cee6caccd92e9541938d87ecf0a
Слава <this-is-yashka@yandex.ru>
2023-06-17 17:34:30 +0500

    Update module_test.py

--- module_test.py
+++ module_test.py
@@ -32,21 +32,19 @@ parser = Parser()
 CSHARP_LANGUAGE = Language('build/my-languages.so', 'c_sharp')
 parser.set_language(CSHARP_LANGUAGE)
 
-
-
 def test_metrics():
     test_data_file = "data/test.csv"
     test_dataset = TextDataset(tokenizer, test_data_file=test_data_file, file_type='test')
     result = test(model, tokenizer, test_dataset, device)
     assert (result['test_f1'] > 0.8)
 
-def file_with_vuls():
+def test_file_with_vuls():
     string_data = file_inner("data/file_with_vuls.cs")
     string_data = obfuscate(parser, string_data)
     result = predict(model, tokenizer, string_data, device, do_linelevel_preds=True)
     assert sum(result['vulnerable']) > 0
 
-def file_without_vuls():
+def test_without_vuls():
     string_data = file_inner("data/file_without_vuls.cs")
     string_data = obfuscate(parser, string_data)
     result = predict(model, tokenizer, string_data, device, do_linelevel_preds=True)
