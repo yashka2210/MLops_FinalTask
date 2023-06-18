b7e9bcd8e5b766e1b947c44d5445aa8b7543c52c
Савелий <113251478+Goolissimo@users.noreply.github.com>
2023-06-19 00:02:06 +0500

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -7,15 +7,16 @@ import tree_sitter
 from tree_sitter import Language, Parser
 import codecs
 
+
 #Функция считывания файла
 def file_inner(path):
     with codecs.open(path, 'r', 'utf-8') as file:
         code = file.read()
     return code
 
+
 #Удаление комментариев в коде, whitespace, приведение к одной строке
 def cleaner1(code):
-    ## Remove code comments
     pat = re.compile(r'(/\*([^*]|(\*+[^*/]))*\*+/)|(//.*)')
     code = re.sub(pat,'',code)
     code = re.sub('\r','',code)
@@ -31,14 +32,16 @@ def subnodes_by_type(node, node_type_pattern = ''):
         return [node]
     nodes = []
     for child in node.children:
-        nodes.extend(subnodes_by_type(child, node_type_pattern = 'method_declaration'))
+        nodes.extend(subnodes_by_type(child, node_type_pattern='method_declaration'))
     return nodes
+
 
 def add_line_delimiter(method):
     method = method.replace(';', ';\n')
     method = method.replace('{', '\n{\n')
     method = method.replace('}', '}\n')
     return method
+
 
 def obfuscate(parser, code, node_type_pattern='method_declaration'):
     code = cleaner1(code)
@@ -56,22 +59,19 @@ def obfuscate(parser, code, node_type_pattern='method_declaration'):
 def main():
     # Загрузка датасета в Pandas
     df = pd.read_csv('data/df_cs_vuls.csv')
-
     # Проверим наличие дубликатов
     duplicateRows = df[df.duplicated()]
-
     # Удаление дубликатов (если есть)
     if len(duplicateRows) > 0:
         df = df.drop_duplicates().reset_index(drop=True)
-
     # Делим данные на тренировочную, тестовую и валидационную выборки
     train, test = train_test_split(df, test_size=0.3, random_state=42)
     test, val = train_test_split(test, test_size=0.5, random_state=42)
-
     #Сохранение файлов
     train.to_csv('data/train.csv', index=False)
     test.to_csv('data/test.csv', index=False)
     val.to_csv('data/val.csv', index=False)
 
+    
 if __name__ == "__main__":
     main()
